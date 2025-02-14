59dff01da65a1a43c7e069620f0110618370a66a
vin905 <vdivanamal@gmail.com>
2025-02-14 03:38:11 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+document.addEventListener("Todos",init);
+
+function init(){
+    loaTask();
+}
+
+function loaTask(){
+    const tasks = JSON.parse(localStorage.getItem("tasks"))||[];
+    tasks.forEach(tasks=> addTaskToDOM(tasks));
+}
+
+function addTask(){
+    const taskInput=document.getElementById("taskInput");
+    const task=taskInput.value.trim();
+    if(task=="")return;
+
+    addTaskToDOM(task);
+    saveTask(task);
+    taskInput.value="";
+}
+ function addTaskToDOM(task) {
+     const taskList=document.getElementById("taskList");
+     const li=document.createElement("li");
+     li.innerHTML=`${task}<button class="remove-btn">Remove</button>`
+     taskList.appendChild(li);
+    
+     li.querySelector(".remove-btn").addEventListener("click", function(){
+        removeTask (task, li);
+     })
+    }
+    
+     function saveTask(task){
+     const tasks = JSON.parse(localStorage.getItem("tasks")) ||[];
+     tasks.push(task);
+     localStorage.setItem("tasks", JSON.stringify(tasks));
+     }
+    
+     function removeTask(task, element) {
+     let tasks=JSON.parse(localStorage.getItem("tasks"));
+     tasks=tasks.filter(t => t!==task);
+     localStorage.setItem("tasks", JSON.stringify(tasks));
+     element.remove();
+    
+ }
